cd9d6a7dfb4a8a6a5507b63f00f248091e901f8d
Neil Roberts <nroberts@igalia.com>
2018-06-26 15:56:08 +0200

    build-win32: Remove references to the Vulkan pkg-config
    Now that the project includes a copy of the Vulkan header there’s no
    need to search for the Vulkan API.

--- build-win32.sh
+++ build-win32.sh
@@ -59,7 +59,6 @@ rm -rf "$INSTALL_DIR"
 rm -rf "$RESULT_DIR" "$RESULT_FILE"
 mkdir -p "$DEPS_DIR"
 mkdir -p "$RESULT_DIR"
-mkdir -p "$INSTALL_DIR/lib/pkgconfig"
 
 function do_download ()
 {
@@ -74,56 +73,10 @@ do_download "$CONFIG_GUESS_URL" "config.guess"
 find_compiler
 BUILD=`bash $DOWNLOADS_DIR/config.guess`
 
-RUN_PKG_CONFIG="$DEPS_DIR/run-pkg-config.sh";
-
-echo "Generating $DEPS_DIR/run-pkg-config.sh";
-
-cat > "$RUN_PKG_CONFIG" <<EOF
-# This is a wrapper script for pkg-config that overrides the
-# PKG_CONFIG_LIBDIR variable so that it won't pick up the local system
-# .pc files.
-
-# The MinGW compiler on Fedora tries to do a similar thing except that
-# it also unsets PKG_CONFIG_PATH. This breaks any attempts to add a
-# local search path so we need to avoid using that script.
-
-export PKG_CONFIG_LIBDIR="$INSTALL_DIR/lib/pkgconfig"
-
-exec pkg-config "\$@"
-EOF
-
-chmod a+x "$RUN_PKG_CONFIG";
-
-echo "Looking for Vulkan SDK in Wine C drive"
-
-vulkan_lib=$(find ~/.wine/drive_c/VulkanSDK \
-                  \( -name vulkan-1.lib -print \) -o \
-                  \( -name Source -prune \) | head -n 1)
-if test -z "$vulkan_lib"; then
-    echo "Couldn't find Vulkan SDK. Try installing it with Wine."
-    exit 1
-fi
-
-vulkan_root=$(cd $(dirname "$vulkan_lib")/.. && pwd)
-
-cat > "$INSTALL_DIR/lib/pkgconfig/vulkan.pc" <<EOF
-prefix=$vulkan_root
-exec_prefix=$vulkan_root
-libdir=\${exec_prefix}/Lib
-includedir=\${prefix}/Include
-
-Name: VULKAN
-Description: Vulkan Loader
-Version: 1.0.61
-Libs: -L\${libdir} -lvulkan-1
-Cflags: -I\${includedir}
-EOF
-
 cd "$BUILD_DIR"
 
 cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCMAKE_C_FLAGS="-mms-bitfields -I$INSTALL_DIR/include -O3" \
-      -DPKG_CONFIG_EXECUTABLE="$RUN_PKG_CONFIG" \
       -DCMAKE_SYSTEM_NAME=Windows \
       -DCMAKE_C_COMPILER=${MINGW_TOOL_PREFIX}gcc \
       -DCMAKE_CXX_COMPILER=${MINGW_TOOL_PREFIX}g++ \
