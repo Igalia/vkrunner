4c6e394e43f3749f827290898b5a92f24fa258dd
Neil Roberts <nroberts@igalia.com>
2019-01-31 02:49:38 +0100

    Add tables for all the features provided by extensions
    All of the extensions that contain structs with features that can be
    retrieved with vkGetPhysicalDeviceFeatures2 are now listed in a table
    with names and offsets into the structs. The tables are generated from
    the Vulkan header using a Python script.
    
    Reviewed-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- Android.mk
+++ Android.mk
@@ -33,6 +33,7 @@ VKRUNNER_SRC_FILES := vkrunner/vr-allocate-store.c \
 	vkrunner/vr-context.c \
 	vkrunner/vr-error-message.c \
 	vkrunner/vr-executor.c \
+	vkrunner/vr-feature.c \
 	vkrunner/vr-feature-offsets.c \
 	vkrunner/vr-flush-memory.c \
 	vkrunner/vr-format.c \
