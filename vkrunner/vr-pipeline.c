0e16724058e9f8f5d7d7ea27bed5083d684d8289
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-02-06 19:31:41 +0100

    pipeline: Move the compiler code to a separate module
    This helps to reduce the code size of the pipeline module and will make
    it easier to port just this part to Rust.

--- vkrunner/vr-pipeline.c
+++ vkrunner/vr-pipeline.c
@@ -26,13 +26,12 @@
 #include "config.h"
 
 #include "vr-pipeline.h"
-#include "vr-subprocess.h"
 #include "vr-util.h"
 #include "vr-script-private.h"
 #include "vr-error-message.h"
 #include "vr-buffer.h"
-#include "vr-temp-file.h"
 #include "vr-format-private.h"
+#include "vr-compiler.h"
 
 #include <stddef.h>
 #include <stdio.h>
@@ -40,24 +39,11 @@
 #include <assert.h>
 #include <string.h>
 #include <limits.h>
-#ifndef WIN32
-#include <unistd.h>
-#endif
 
 struct desc_set_bindings_info {
         VkDescriptorSetLayoutBinding *bindings;
         unsigned n_bindings;
         unsigned desc_set;
-};
-
-static const char *
-stage_names[VR_SHADER_STAGE_N_STAGES] = {
-        [VR_SHADER_STAGE_VERTEX] = "vert",
-        [VR_SHADER_STAGE_TESS_CTRL] = "tesc",
-        [VR_SHADER_STAGE_TESS_EVAL] = "tese",
-        [VR_SHADER_STAGE_GEOMETRY] = "geom",
-        [VR_SHADER_STAGE_FRAGMENT] = "frag",
-        [VR_SHADER_STAGE_COMPUTE] = "comp",
 };
 
 static const VkPipelineMultisampleStateCreateInfo
@@ -66,356 +52,6 @@ base_multisample_state = {
         .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT
 };
 
-static char *
-create_file_for_shader(const struct vr_config *config,
-                       const struct vr_script_shader_code *shader)
-{
-        char *filename;
-        FILE *out;
-
-        if (!vr_temp_file_create_named(config, &out, &filename))
-                return NULL;
-
-        fwrite(shader->source, 1, shader->source_length, out);
-
-        fclose(out);
-
-        return filename;
-}
-
-static bool
-load_stream_contents(const struct vr_config *config,
-                     FILE *stream,
-                     uint8_t **contents_out,
-                     size_t *size_out)
-{
-        size_t got;
-        long pos;
-
-        fseek(stream, 0, SEEK_END);
-        pos = ftell(stream);
-
-        if (pos == -1) {
-                vr_error_message(config, "ftell failed");
-                return false;
-        }
-
-        size_t size = pos;
-        rewind(stream);
-        uint8_t *contents = vr_alloc(size);
-
-        got = fread(contents, 1, size, stream);
-        if (got != size) {
-                vr_error_message(config, "Error reading file contents");
-                vr_free(contents);
-                return false;
-        }
-
-        *contents_out = contents;
-        *size_out = size;
-
-        return true;
-}
-
-static bool
-show_disassembly(const struct vr_config *config,
-                 const char *filename)
-{
-        char *args[] = {
-                getenv("PIGLIT_SPIRV_DIS_BINARY"),
-                (char *) filename,
-                NULL
-        };
-
-        if (args[0] == NULL)
-                args[0] = "spirv-dis";
-
-        return vr_subprocess_command(config, args);
-}
-
-static VkShaderModule
-compile_stage(const struct vr_config *config,
-              struct vr_window *window,
-              const struct vr_script *script,
-              enum vr_shader_stage stage,
-              const struct vr_script_shader_code *shaders,
-              size_t total_n_shaders)
-{
-        const struct vr_vk_device *vkfn = vr_window_get_vkdev(window);
-        const int n_base_args = 8;
-        VkShaderModule module = VK_NULL_HANDLE;
-        FILE *module_stream = NULL;
-        char *module_filename;
-        uint8_t *module_binary = NULL;
-        size_t module_size;
-        bool res;
-        char version_str[64];
-        const struct vr_requirements *requirements =
-                vr_script_get_requirements(script);
-        uint32_t version = vr_requirements_get_version(requirements);
-
-        int n_shaders = 0;
-
-        for (unsigned i = 0; i < total_n_shaders; i++) {
-                if (shaders[i].stage == stage)
-                        n_shaders++;
-        }
-
-        char **args = alloca((n_base_args + n_shaders + 1) * sizeof args[0]);
-
-        sprintf(version_str, "vulkan%u.%u", VK_VERSION_MAJOR(version),
-                VK_VERSION_MINOR(version));
-
-        memset(args + n_base_args, 0, (n_shaders + 1) * sizeof args[0]);
-
-        if (!vr_temp_file_create_named(config,
-                                       &module_stream,
-                                       &module_filename))
-                goto out;
-
-        args[0] = getenv("PIGLIT_GLSLANG_VALIDATOR_BINARY");
-        if (args[0] == NULL)
-                args[0] = "glslangValidator";
-
-        args[1] = "-V";
-        args[2] = "--target-env";
-        args[3] = version_str;
-        args[4] = "-S";
-        args[5] = (char *) stage_names[stage];
-        args[6] = "-o";
-        args[7] = module_filename;
-
-        int i = n_base_args;
-        for (unsigned shader_num = 0;
-             shader_num < total_n_shaders;
-             shader_num++) {
-                if (shaders[shader_num].stage != stage)
-                        continue;
-
-                args[i] = create_file_for_shader(config, shaders + shader_num);
-                if (args[i] == 0)
-                        goto out;
-                i++;
-        }
-
-        res = vr_subprocess_command(config, args);
-        if (!res) {
-                vr_error_message(config, "glslangValidator failed");
-                goto out;
-        }
-
-        if (config->show_disassembly)
-                show_disassembly(config, module_filename);
-
-        if (!load_stream_contents(config,
-                                  module_stream,
-                                  &module_binary,
-                                  &module_size))
-                goto out;
-
-        VkShaderModuleCreateInfo shader_module_create_info = {
-                        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-                        .codeSize = module_size,
-                        .pCode = (const uint32_t *) module_binary
-        };
-        res = vkfn->vkCreateShaderModule(vr_window_get_device(window),
-                                         &shader_module_create_info,
-                                         NULL, /* allocator */
-                                         &module);
-        if (res != VK_SUCCESS) {
-                vr_error_message(config, "vkCreateShaderModule failed");
-                module = VK_NULL_HANDLE;
-                goto out;
-        }
-
-out:
-        for (i = 0; i < n_shaders; i++) {
-                if (args[i + n_base_args]) {
-                        unlink(args[i + n_base_args]);
-                        vr_free(args[i + n_base_args]);
-                }
-        }
-
-        if (module_stream) {
-                fclose(module_stream);
-                unlink(module_filename);
-                vr_free(module_filename);
-        }
-
-        if (module_binary)
-                vr_free(module_binary);
-
-        return module;
-}
-
-static VkShaderModule
-assemble_stage(const struct vr_config *config,
-               struct vr_window *window,
-               const struct vr_script *script,
-               const struct vr_script_shader_code *shader)
-{
-        const struct vr_vk_device *vkfn = vr_window_get_vkdev(window);
-        FILE *module_stream = NULL;
-        char *module_filename;
-        char *source_filename = NULL;
-        uint8_t *module_binary = NULL;
-        VkShaderModule module = VK_NULL_HANDLE;
-        size_t module_size;
-        bool res;
-        char version_str[64];
-        const struct vr_requirements *requirements =
-                vr_script_get_requirements(script);
-        uint32_t version = vr_requirements_get_version(requirements);
-
-        sprintf(version_str, "vulkan%u.%u", VK_VERSION_MAJOR(version),
-                VK_VERSION_MINOR(version));
-
-        if (!vr_temp_file_create_named(config,
-                                       &module_stream,
-                                       &module_filename))
-                goto out;
-
-        source_filename = create_file_for_shader(config, shader);
-        if (source_filename == NULL)
-                goto out;
-
-        char *args[] = {
-                getenv("PIGLIT_SPIRV_AS_BINARY"),
-                "--target-env", version_str,
-                "-o", module_filename,
-                source_filename,
-                NULL
-        };
-
-        if (args[0] == NULL)
-                args[0] = "spirv-as";
-
-        res = vr_subprocess_command(config, args);
-        if (!res) {
-                vr_error_message(config, "spirv-as failed");
-                goto out;
-        }
-
-        if (config->show_disassembly)
-                show_disassembly(config, module_filename);
-
-        if (!load_stream_contents(config,
-                                  module_stream,
-                                  &module_binary,
-                                  &module_size))
-                goto out;
-
-        VkShaderModuleCreateInfo shader_module_create_info = {
-                        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-                        .codeSize = module_size,
-                        .pCode = (const uint32_t *) module_binary
-        };
-        res = vkfn->vkCreateShaderModule(vr_window_get_device(window),
-                                         &shader_module_create_info,
-                                         NULL, /* allocator */
-                                         &module);
-        if (res != VK_SUCCESS) {
-                vr_error_message(config, "vkCreateShaderModule failed");
-                module = VK_NULL_HANDLE;
-                goto out;
-        }
-
-out:
-        if (source_filename) {
-                unlink(source_filename);
-                vr_free(source_filename);
-        }
-
-        if (module_stream) {
-                fclose(module_stream);
-                unlink(module_filename);
-                vr_free(module_filename);
-        }
-
-        if (module_binary)
-                vr_free(module_binary);
-
-        return module;
-}
-
-static VkShaderModule
-load_binary_stage(const struct vr_config *config,
-                  struct vr_window *window,
-                  const struct vr_script_shader_code *shader)
-{
-        const struct vr_vk_device *vkfn = vr_window_get_vkdev(window);
-        VkShaderModule module = VK_NULL_HANDLE;
-        bool res;
-
-        if (config->show_disassembly) {
-                FILE *module_stream;
-                char *module_filename;
-
-                if (vr_temp_file_create_named(config,
-                                              &module_stream,
-                                              &module_filename)) {
-                        fwrite(shader->source,
-                               1, shader->source_length,
-                               module_stream);
-                        fclose(module_stream);
-
-                        show_disassembly(config, module_filename);
-
-                        unlink(module_filename);
-                        vr_free(module_filename);
-                }
-        }
-
-        VkShaderModuleCreateInfo shader_module_create_info = {
-                        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-                        .codeSize = shader->source_length,
-                        .pCode = (const uint32_t *) shader->source
-        };
-        res = vkfn->vkCreateShaderModule(vr_window_get_device(window),
-                                         &shader_module_create_info,
-                                         NULL, /* allocator */
-                                         &module);
-        if (res != VK_SUCCESS)
-                vr_error_message(config, "vkCreateShaderModule failed");
-
-        return module;
-}
-
-static VkShaderModule
-build_stage(const struct vr_config *config,
-            struct vr_window *window,
-            const struct vr_script *script,
-            enum vr_shader_stage stage,
-            const struct vr_script_shader_code *shaders,
-            size_t total_n_shaders)
-{
-        const struct vr_script_shader_code *first_shader = NULL;
-
-        for (unsigned i = 0; i < total_n_shaders; i++) {
-                if (shaders[i].stage == stage) {
-                        first_shader = shaders + i;
-                        break;
-                }
-        }
-
-        assert(first_shader != NULL);
-
-        switch (first_shader->source_type) {
-        case VR_SCRIPT_SOURCE_TYPE_GLSL:
-                return compile_stage(config,
-                                     window,
-                                     script,
-                                     stage,
-                                     shaders,
-                                     total_n_shaders);
-        case VR_SCRIPT_SOURCE_TYPE_SPIRV:
-                return assemble_stage(config, window, script, first_shader);
-        case VR_SCRIPT_SOURCE_TYPE_BINARY:
-                return load_binary_stage(config, window, first_shader);
-        }
-
-        vr_fatal("should not be reached");
-}
 
 static void
 set_up_attrib_cb(const struct vr_vbo_attrib *attrib,
@@ -891,12 +527,10 @@ vr_pipeline_create(const struct vr_config *config,
                 script_stages |= shader_bit;
 
                 pipeline->modules[shader->stage] =
-                        build_stage(config,
-                                    window,
-                                    script,
-                                    shader->stage,
-                                    shaders,
-                                    n_shaders);
+                        vr_compiler_build_stage(config,
+                                                window,
+                                                script,
+                                                shader->stage);
                 if (pipeline->modules[shader->stage] == VK_NULL_HANDLE)
                         goto error;
         }
