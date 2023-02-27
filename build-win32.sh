78b0c4137d60d2e70aaf3a018e662070144defa3
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-02-27 14:44:47 +0100

    build-win32.sh: Convert to building with Meson/Rust

--- build-win32.sh
+++ build-win32.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-CONFIG_GUESS_URL="http://git.savannah.gnu.org/gitweb/?p=automake.git;a=blob_plain;f=lib/config.guess"
-
 SRC_DIR=$(cd $(dirname "$0") && pwd)
 BUILD_DIR="$SRC_DIR/build-win32"
-DOWNLOADS_DIR="$BUILD_DIR/downloads"
 DEPS_DIR="$BUILD_DIR/deps"
 INSTALL_DIR="$BUILD_DIR/install"
 RESULT_FILE="$BUILD_DIR/vkrunner.zip"
@@ -52,8 +49,6 @@ function find_compiler ()
     echo "Using compiler ${MINGW_TOOL_PREFIX}gcc and target $TARGET";
 }
 
-mkdir -p "$DOWNLOADS_DIR"
-
 rm -rf "$DEPS_DIR"
 rm -rf "$INSTALL_DIR"
 rm -rf "$RESULT_DIR" "$RESULT_FILE"
@@ -61,76 +56,34 @@ mkdir -p "$DEPS_DIR"
 mkdir -p "$RESULT_DIR"
 mkdir -p "$INSTALL_DIR/lib/pkgconfig"
 
-function do_download ()
-{
-    local local_fn="$DOWNLOADS_DIR/$2"
-    if ! test -f "$local_fn"; then
-        curl -L -o "$local_fn" "$1"
-    fi
-}
+CROSS_FILE="$DEPS_DIR/windows.cross"
 
-do_download "$CONFIG_GUESS_URL" "config.guess"
+cat > "$CROSS_FILE" <<EOF
+[binaries]
+c = '${MINGW_TOOL_PREFIX}gcc'
+cpp = '${MINGW_TOOL_PREFIX}g++'
+ar = '${MINGW_TOOL_PREFIX}ar'
+strip = '${MINGW_TOOL_PREFIX}strip'
+rust = 'rustc'
+exe_wrapper = 'wine64'
+
+[host_machine]
+system = 'windows'
+cpu_family = 'x86_64'
+cpu = 'x86_64'
+endian = 'little'
+EOF
 
 find_compiler
-BUILD=`bash $DOWNLOADS_DIR/config.guess`
 
-RUN_PKG_CONFIG="$DEPS_DIR/run-pkg-config.sh";
-
-echo "Generating $DEPS_DIR/run-pkg-config.sh";
-
-cat > "$RUN_PKG_CONFIG" <<EOF
-# This is a wrapper script for pkg-config that overrides the
-# PKG_CONFIG_LIBDIR variable so that it won't pick up the local system
-# .pc files.
-
-# The MinGW compiler on Fedora tries to do a similar thing except that
-# it also unsets PKG_CONFIG_PATH. This breaks any attempts to add a
-# local search path so we need to avoid using that script.
-
-export PKG_CONFIG_LIBDIR="$INSTALL_DIR/lib/pkgconfig"
-
-exec pkg-config "\$@"
-EOF
-
-chmod a+x "$RUN_PKG_CONFIG";
-
-echo "Looking for Vulkan SDK in Wine C drive"
-
-vulkan_lib=$(find ~/.wine/drive_c/VulkanSDK \
-                  \( -name vulkan-1.lib -print \) -o \
-                  \( -name Source -prune \) | head -n 1)
-if test -z "$vulkan_lib"; then
-    echo "Couldn't find Vulkan SDK. Try installing it with Wine."
-    exit 1
-fi
-
-vulkan_root=$(cd $(dirname "$vulkan_lib")/.. && pwd)
-
-cat > "$INSTALL_DIR/lib/pkgconfig/vulkan.pc" <<EOF
-prefix=$vulkan_root
-exec_prefix=$vulkan_root
-libdir=\${exec_prefix}/Lib
-includedir=\${prefix}/Include
-
-Name: VULKAN
-Description: Vulkan Loader
-Version: 1.0.61
-Libs: -L\${libdir} -lvulkan-1
-Cflags: -I\${includedir}
-EOF
-
-cd "$BUILD_DIR"
-
-cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -DCMAKE_C_FLAGS="-mms-bitfields -I$INSTALL_DIR/include -O3" \
-      -DPKG_CONFIG_EXECUTABLE="$RUN_PKG_CONFIG" \
-      -DCMAKE_SYSTEM_NAME=Windows \
-      -DCMAKE_C_COMPILER=${MINGW_TOOL_PREFIX}gcc \
-      -DCMAKE_CXX_COMPILER=${MINGW_TOOL_PREFIX}g++ \
+meson setup --cross-file "$CROSS_FILE" \
+      -Drust_args="--target x86_64-pc-windows-gnu -C linker=\"${MINGW_TOOL_PREFIX}gcc\"" \
+      -Dprefix="$INSTALL_DIR" \
+      "$BUILD_DIR" \
       "$SRC_DIR"
 
-make -j4
-make install
+ninja -C "$BUILD_DIR"
+ninja -C "$BUILD_DIR" install
 
 cp "$INSTALL_DIR/bin/vkrunner.exe" "$RESULT_DIR"
 cp -r "$SRC_DIR/examples" "$RESULT_DIR"
